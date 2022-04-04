0d5464c5c02e7e4b751a8d8d57ef236193952808
gbouras13 <george.bouras@adelaide.edu.au>
2022-04-04 16:38:46 +0930

    motif finder

---
+++ motif_finder.py
@@ -0,0 +1,32 @@
+import argparse
+from argparse import RawTextHelpFormatter
+from Bio.Seq import Seq
+import Bio.motifs as motifs
+from Bio import SeqIO
+
+
+
+def get_input():
+	usage = 'python3 motif_finder.py ...'
+	parser = argparse.ArgumentParser(description='script to output coordinates of motif in a fasta file', formatter_class=RawTextHelpFormatter)
+	parser.add_argument('-i', '--infile', action="store", help='input file in fasta format',  required=True)
+	parser.add_argument('-m', '--motif', action="store", help='find motif',  required=True)
+	#parser.add_argument('-o', '--outfile', action="store", help='output file',  required=True)
+	args = parser.parse_args()
+
+	return args
+
+args = get_input()
+
+record = SeqIO.read(args.infile, "fasta")
+motif = args.motif
+
+
+instances = [Seq(motif)]
+m = motifs.create(instances)
+
+reads = list(SeqIO.parse(args.infile, "fasta"))
+
+for i in range(len(reads)):
+    for pos, seq in m.instances.search(reads[i].seq):
+        print("%i %s" % (pos, seq))
