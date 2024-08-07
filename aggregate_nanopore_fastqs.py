aa9e873848ae1a01355fafef9f0fbbcdd246b262
gbouras13 <george.bouras@adelaide.edu.au>
2024-08-07 14:45:41 +0930

    handle dorado rebasecalled reads 3

--- aggregate_nanopore_fastqs.py
+++ aggregate_nanopore_fastqs.py
@@ -16,15 +16,15 @@ If you have samples with multiple runs, the code will detect and append to the s
 
 
 def get_input():
-	usage = 'python3 aggregate_nanopore_fastqs.py ...'
-	parser = argparse.ArgumentParser(description='script to aggreagted nanopore FASTQs and label with sample name. Will append already existing files, so be careful!', formatter_class=RawTextHelpFormatter)
-	parser.add_argument('-i', '--infile', action="store", help='input csv file in csv format. 2 columns with sample names an barcode (integers). Needs 2 columns: Sample and Barcode. Example Sample,Barcode\nsample,01 ',  required=True)
-	parser.add_argument('-d', '--directory', action="store", help='fastq_pass directory path',  required=True)
-	parser.add_argument('-o', '--output', action="store", help='output directory where the aggregated FASTQs will be saved',  required=True)
-        parser.add_argument('--dorado', action="store_true", help='use if dorado rebasecalled, will be only 1 fastq')
-	args = parser.parse_args()
+    usage = 'python3 aggregate_nanopore_fastqs.py ...'
+    parser = argparse.ArgumentParser(description='script to aggreagted nanopore FASTQs and label with sample name. Will append already existing files, so be careful!', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-i', '--infile', action="store", help='input csv file in csv format. 2 columns with sample names an barcode (integers). Needs 2 columns: Sample and Barcode. Example Sample,Barcode\nsample,01 ',  required=True)
+    parser.add_argument('-d', '--directory', action="store", help='fastq_pass directory path',  required=True)
+    parser.add_argument('-o', '--output', action="store", help='output directory where the aggregated FASTQs will be saved',  required=True)
+    parser.add_argument('--dorado', action="store_true", help='use if dorado rebasecalled, will be only 1 fastq')
+    args = parser.parse_args()
 
-	return args
+    return args
 
 args = get_input()
 
@@ -48,12 +48,12 @@ for sample_id, barcode in data_dict.items():
     # Create the output file path
     output_file = os.path.join(output_dir, f'{sample_id}.fastq.gz')
 
-    if dorado is True:
+    if args.dorado is True:
 
         # Find and concatenate the corresponding .fastq.gz files
         fastq_files = [f for f in os.listdir(fastq_path) if f.endswith('.fastq.gz') and barcode in f]
         if len(fastq_files) == 0:
-      	    raise FileNotFoundError(f"No fastq file containing the barcode '{barcode}' found in {fastq_path}")
+            raise FileNotFoundError(f"No fastq file containing the barcode '{barcode}' found in {fastq_path}")
         else:
             barcode_path = fastq_path
             for f in fastq_files:
